4636aa356cd716bae61c7f66b65c0550a9ab1b40
notduyly <162235323+notduyly@users.noreply.github.com>
2024-09-20 17:25:55 -0700

    Add the ability to create a bot and .gitignore
    1) Implement the bot to be added to a Discord server.
        - The bot can take a command with the prefix '!'
        - The command !hello will allow the bot to greet the user
    2) Add .gitignore.
        -The bot's unique token is stored in a local file. So exclude this file from the repository

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import discord
 from discord.ext import commands
 
+intent = discord.Intents.default()
+intent.message_content = True
+client = commands.Bot(command_prefix= '!', intents=intent)
+
+
+@client.event
+async def on_ready(): #this function will execute when it is 'ready'
+    print("Bot is ready!")
+    print("-------------")
+    print()
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hi, I am Gizmo")
+
+
+
+#the unique bot token is stored in a local txt
+try:
+    with open('token.txt') as file:
+        token = file.read()
+except FileNotFoundError:
+    FileNotFoundError
+client.run(token)
