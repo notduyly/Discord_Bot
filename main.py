80f3b50f23feabbc06beeaa9aa1db5762108a928
notduyly <162235323+notduyly@users.noreply.github.com>
2024-09-22 14:17:30 -0700

    Add commands such as hello, join/leave a voice channel, and emoji output
    1) .hello
        - The bot can now greet the user.
    2) Member Join
        - When a new user joins the discord the bot will greet them.
    3) .join/.leave
        - The bot can join and leave the voice channel.
         - If the user is not in a voice channel the bot will output stating they "Must be in a voice channel to run this command" (same concept applies with leave)
    4) Emoji output
        - Text output includes Discord's emoji to foster a more friendly connection.

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import discord
 from discord.ext import commands
+import asyncio
+
+#sensitive info such as (token, channel ID)
+#this file is in .gitignore
+try:
+    with open('token.txt') as file:
+        token = file.readline()
+        channel_id = int(file.readline())
+except FileNotFoundError:
+    FileNotFoundError
+
 
 intent = discord.Intents.default()
 intent.message_content = True
-client = commands.Bot(command_prefix= '!', intents=intent)
+intent.members = True
+client = commands.Bot(command_prefix= '.', intents=intent)
 
 
 @client.event
-async def on_ready(): #this function will execute when it is 'ready'
-    print("Bot is ready!")
+async def on_ready():
+    print("We are online and ready!")
     print("-------------")
     print()
 
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(channel_id)
+    await channel.send("Welcome :wave:, I hope you enjoy your stay :blush:")
+
+
 @client.command()
 async def hello(ctx):
-    await ctx.send("Hi, I am Gizmo")
+    await ctx.send("Hi, I am Gizmo :smile:")
+
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("My apologies, I detected that you're not in a voice channel :sweat_smile: \n\
+You must be in a voice channel to run this command")
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.send("As you wish, goodbye :slight_smile:")
+        await asyncio.sleep(1)
+        await ctx.guild.voice_client.disconnect()
+        
+    else:
+        await ctx.send("I am currently not in a voice channel")
 
 
-
-#the unique bot token is stored in a local txt
-try:
-    with open('token.txt') as file:
-        token = file.read()
-except FileNotFoundError:
-    FileNotFoundError
 client.run(token)
