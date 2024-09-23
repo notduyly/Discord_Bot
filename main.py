17848fefcd93e25ff687179804755f96b78f1484
notduyly <162235323+notduyly@users.noreply.github.com>
2024-09-23 00:32:02 -0700

    Add a wake word to bot, and cleaned up redundancy

--- main.py
+++ main.py
@@ -1,49 +1,53 @@
 import discord
 from discord.ext import commands
 import asyncio
-
-#sensitive info such as (token, channel ID)
-#this file is in .gitignore
-try:
-    with open('token.txt') as file:
-        token = file.readline()
-        channel_id = int(file.readline())
-except FileNotFoundError:
-    FileNotFoundError
+from config import *
 
 
 intent = discord.Intents.default()
 intent.message_content = True
 intent.members = True
-client = commands.Bot(command_prefix= '.', intents=intent)
+bot = commands.Bot(command_prefix = '.', intents = intent)
 
 
-@client.event
+@bot.event
 async def on_ready():
     print("We are online and ready!")
     print("-------------")
     print()
 
-@client.event
+@bot.event
 async def on_member_join(member):
-    channel = client.get_channel(channel_id)
+    channel = bot.get_channel(general_txt_channel)
     await channel.send("Welcome :wave:, I hope you enjoy your stay :blush:")
 
+@bot.event
+async def on_message(message):
+        
+    if wake_word in message.content.lower() and message.author != bot.user:
+        await message.channel.send("Sorry for the inconvenience :sweat_smile: \n\
+:construction: My AI support function is under construction :construction: ")
+    else: 
+        await bot.process_commands(message)    
+        
 
-@client.command()
+
+
+
+@bot.command()
 async def hello(ctx):
     await ctx.send("Hi, I am Gizmo :smile:")
 
-@client.command(pass_context = True)
+@bot.command()
 async def join(ctx):
     if (ctx.author.voice):
-        channel = ctx.message.author.voice.channel
-        await channel.connect()
+        v_channel = ctx.message.author.voice.channel
+        await v_channel.connect()
     else:
         await ctx.send("My apologies, I detected that you're not in a voice channel :sweat_smile: \n\
 You must be in a voice channel to run this command")
 
-@client.command(pass_context = True)
+@bot.command()
 async def leave(ctx):
     if (ctx.voice_client):
         await ctx.send("As you wish, goodbye :slight_smile:")
@@ -54,4 +58,7 @@ async def leave(ctx):
         await ctx.send("I am currently not in a voice channel")
 
 
-client.run(token)
+
+
+
+bot.run(bot_token)
