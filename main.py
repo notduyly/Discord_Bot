dbf0b2b1c46ebcb2f830c2aa28527aff6c175541
notduyly <162235323+notduyly@users.noreply.github.com>
2024-09-24 21:07:40 -0700

    Add ability to kick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
-from discord.ext import commands
 import asyncio
+from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
+from discord import Member
 from config import *
 
 
@@ -17,16 +19,16 @@ async def on_ready():
     print()
 
 @bot.event
-async def on_member_join(member):
+async def on_member_join(member: discord.Member):
     channel = bot.get_channel(general_txt_channel)
-    await channel.send("Welcome :wave:, I hope you enjoy your stay :blush:")
+    await channel.send(f"Welcome **{member.display_name}** :wave:, I hope you enjoy your stay! :blush:")
 
 @bot.event
 async def on_message(message):
         
     if wake_word in message.content.lower() and message.author != bot.user:
         await message.channel.send("Sorry for the inconvenience :sweat_smile: \n\
-:construction: My AI support function is under construction :construction: ")
+:construction: **My AI support function is under construction** :construction: ")
     else: 
         await bot.process_commands(message)    
         
@@ -45,7 +47,7 @@ async def join(ctx):
         await v_channel.connect()
     else:
         await ctx.send("My apologies, I detected that you're not in a voice channel :sweat_smile: \n\
-You must be in a voice channel to run this command")
+**You must be in a voice channel to run this command**")
 
 @bot.command()
 async def leave(ctx):
@@ -57,8 +59,15 @@ async def leave(ctx):
     else:
         await ctx.send("I am currently not in a voice channel")
 
-
-
+@bot.command()
+@has_permissions(kick_members = True)
+async def kick(ctx, member: discord.Member, *, reason= None):
+    await member.kick(reason = reason)
+    await ctx.send(f"{member} has been kicked from the server.")
+@kick.error
+async def kick_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You don't have permission to kick people!")
 
 
 bot.run(bot_token)
